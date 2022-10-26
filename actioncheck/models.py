864e63c03afce120d05371ce89aa9c7a7253f4af
Prem-Vk <prem.vishwakarma@instamojo.com>
2022-10-26 20:58:50 +0530

    Updated migration file

--- actioncheck/models.py
+++ actioncheck/models.py
@@ -1,5 +1,7 @@
+from email.policy import default
 from django.db import models
 
 class Test1(models.Model):
     show_on_store = models.BooleanField(default=True)
     note = models.TextField(null=True, blank=True)
+    married = models.BooleanField(default=False)
