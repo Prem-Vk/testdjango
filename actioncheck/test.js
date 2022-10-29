a2bfc94f1d57061e46b8cb902da7aa726e80811b
Prem-Vk <prem.vishwakarma@instamojo.com>
2022-10-29 17:45:52 +0530

    Added github actions file

---
+++ actioncheck/test.js
@@ -0,0 +1,65 @@
+const commentHeading = "Migration file detected for PR number: ";
+const pr_number = context.issue.number;
+let filenames = [];
+
+function commentCreator(filenames, pr_number) {
+  github.rest.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: `${filenames} ${commentHeading} ${pr_number}`,
+  });
+}
+
+function commentDeleter(comment_id) {
+  github.rest.issues.deleteComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    comment_id: `${comment_id}`,
+  });
+}
+
+function getPullRequestComments() {
+  return github.rest.issues.listComments({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+  });
+}
+
+async function checkComments(all_comments) {
+  const format_all_comments = await github.paginate(all_comments);
+  for (const comment of format_all_comments) {
+    let comment_body = comment.body;
+    if (comment_body.includes(`${commentHeading} ${pr_number}`)) {
+      return String(comment.url);
+    } else {
+      return false;
+    }
+  }
+}
+
+async function listAllPullRequestFiles() {
+  return await github.rest.pulls.listFiles({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.issue.number,
+  });
+}
+const pulls = await listAllPullRequestFiles();
+const issues = await github.paginate(pulls);
+for (const issue of issues) {
+  let filename = String(issue.filename);
+  if (filename.includes("migrations")) {
+    filenames.push(filename);
+  }
+}
+const all_comments = await getPullRequestComments();
+const migration_comment_available = await checkComments(all_comments);
+
+if (filenames && typeof migration_comment_available === "boolean") {
+  commentCreator(filenames, pr_number);
+} else if (typeof migration_comment_available === "string") {
+  let comment_id = parseInt(migration_comment_available.split("/").pop());
+  commentDeleter(comment_id);
+}
---
+++ actioncheck/test2.js
@@ -0,0 +1,3 @@
+let text = "https://api.github.com/repos/Prem-Vk/testdjango/issues/comments/1293025443"
+const myArray = text.split("/").pop();
+console.log(myArray)
